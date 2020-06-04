1d7ad68a91aed9f4fdde9efd7b4873f2dbbaaa51
taren <taren.lewis@gmail.com>
2020-06-04 10:47:57 -0500

    Back Button causes fragment pop off to title screen

--- andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -18,23 +18,24 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
 
-
+        /*
         // An optional way to create the on click listener action, with Navigation. There are alternative methods located below.
         binding.playButton.setOnClickListener {
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        }
+        }*/
 
-        /*
         binding.playButton.setOnClickListener { view: View ->
 
             // This line can be replaced, via Jetpack project Android KTX extension functions, which allow a class to extend the functionality of
             // an existing class, without subclassing it. KTX has an extension function for the View class. This is the original line:
-            // Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+             Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
             // and the replacement:
-            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
-        }*/
+            //view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+
+        }
 
         return binding.root
 
